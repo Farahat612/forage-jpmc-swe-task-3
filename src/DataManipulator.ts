d57273d6f234c129aa9e6ea3516cc7fc18caefd3
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2023-06-05 13:29:55 +0300

    Code changes to meet the task objectives.

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,36 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
   timestamp: Date,
+  price_abc: number; 
+  price_def: number;
+  ratio: number;
+  upper_bound: number;
+  lower_bound: number;
+  trigger_alert?: number; 
 }
-
 
 export class DataManipulator {
   static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+    const [abc, def] = serverResponds;
+    const price_abc = abc.top_ask && abc.top_ask.price || 0;
+    const price_def = def.top_ask && def.top_ask.price || 0;
+    const ratio = price_abc / price_def;
+    const upper_bound = 1.05; 
+    const lower_bound = 0.95;
+    let trigger_alert;
+    if (ratio > upper_bound || ratio < lower_bound) {
+      trigger_alert = ratio; 
+    }
+
+    return {
+      timestamp: abc.timestamp,
+      price_abc, 
+      price_def,
+      ratio,
+      upper_bound,
+      lower_bound,
+      trigger_alert,
+    };
   }
 }
